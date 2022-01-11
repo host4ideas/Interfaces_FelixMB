010b3d3ee880975adb86bf44c219bb632db97958
host4ideas <72515482+host4ideas@users.noreply.github.com>
2022-01-11 18:03:53 +0100

    Update colisiones.js

--- Unidad_04/Ejercicios y prácticas/(1) Material para ejercicios Animaciones/Ejercicio 3-4/colisiones.js
+++ Unidad_04/Ejercicios y prácticas/(1) Material para ejercicios Animaciones/Ejercicio 3-4/colisiones.js
@@ -1,12 +1,13 @@
-const leftInput = $("#left");
+const leftInput = $("#left").get(0);
 const rightInput = $("#right").get(0);
 const topInput = $("#top").get(0);
 const bottomInput = $("#bottom").get(0);
 const draggableSquare = $("#draggableSquare").eq(0);
 
-rightInput.onclick = () => setTimeout(draggableSquare.moveElement, 1000, "left", "15");
-leftInput.onclick = () => setTimeout(draggableSquare.moveElement, 1000, "right", "15");
-
 const moveElement = (direction, pixels) => {
-    this.css(direction, `+=${pixels}`)
+    console.log("test");
+    draggableSquare.css(direction, `+=15`)
 }
+
+rightInput.onclick = () => setTimeout(moveElement, 1000, "left", "15");
+leftInput.onclick = () => setTimeout(moveElement, 1000, "right", "15");

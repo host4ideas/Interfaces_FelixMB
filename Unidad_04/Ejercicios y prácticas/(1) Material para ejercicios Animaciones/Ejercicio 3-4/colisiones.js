dbbfc146796d14845e18555822689621664bf69d
host4ideas <felixmb99@hotmail.es>
2022-01-13 17:06:39 +0100

    exercise 4

--- Unidad_04/Ejercicios y prácticas/(1) Material para ejercicios Animaciones/Ejercicio 3-4/colisiones.js
+++ Unidad_04/Ejercicios y prácticas/(1) Material para ejercicios Animaciones/Ejercicio 3-4/colisiones.js
@@ -2,12 +2,30 @@ const leftInput = $("#left").get(0);
 const rightInput = $("#right").get(0);
 const topInput = $("#top").get(0);
 const bottomInput = $("#bottom").get(0);
-const draggableSquare = $("#draggableSquare").eq(0);
+const inputPixels = $("#mov").get(0);
+const draggableSquare = $("#draggableSquare").get(0);
 
 const moveElement = (direction, pixels) => {
-    console.log("test");
-    draggableSquare.css(direction, `+=15`)
+    draggableSquare.css(direction, `+=${pixels}`)
 }
 
-rightInput.onclick = () => setTimeout(moveElement, 1000, "left", "15");
-leftInput.onclick = () => setTimeout(moveElement, 1000, "right", "15");
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    console.log("test");
+    ev.dataTransfer.setData("square", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("square");
+    $(`#${data}`).css("background-color", "red");
+}
+
+rightInput.onclick = () => setTimeout(moveElement, 1000, "left", inputPixels.value);
+leftInput.onclick = () => setTimeout(moveElement, 1000, "left", -inputPixels.value);
+topInput.onclick = () => setTimeout(moveElement, 1000, "top", -inputPixels.value);
+bottomInput.onclick = () => setTimeout(moveElement, 1000, "top", inputPixels.value);
+

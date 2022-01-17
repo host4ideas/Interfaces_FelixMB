5aa7171fa2ff9b53c036a45ffaf7ecf6ce0b912f
host4ideas <felixmb99@hotmail.es>
2022-01-17 20:42:41 +0100

    Update index.js

--- Unidad_04/Ejercicios y prácticas/(1) Material para ejercicios Animaciones/Ejercicio 5/index.js
+++ Unidad_04/Ejercicios y prácticas/(1) Material para ejercicios Animaciones/Ejercicio 5/index.js
@@ -1,6 +1,4 @@
-let clicked = false;
-let counter = -1;
-let timer;
+let counter = 0;
 const mainContainer = document.getElementById("mainContainer");
 
 const randomColor = () => {
@@ -17,38 +15,12 @@ const randomColor = () => {
  */
 
 const animationFunction = () => {
-    return new Promise((resolve, reject) => {
-        timer = setInterval(() => {
-            if (counter < 5) {
-                var newDiv = document.createElement("div");
+    var newDiv = document.createElement("div");
 
-                newDiv.style.height = "50px";
-                newDiv.style.backgroundColor = randomColor();
-                newDiv.style.margin = "5px";
-                counter++;
-
-                newDiv.onclick = () => {
-                    if (clicked) {
-                        // Restart timeout
-                        clicked = false;
-                        counter = 0;
-                        // Remove all the childs
-                        clearDiv();
-                        callbackFunction();
-                    } else {
-                        // Stop timeout
-                        clicked = true;
-                        counter = 0;
-                        clearInterval(timer);
-                    }
-                };
-
-                mainContainer.appendChild(newDiv);
-            }
-
-            resolve();
-        }, 1000);
-    });
+    newDiv.style.height = "50px";
+    newDiv.style.backgroundColor = randomColor();
+    newDiv.style.margin = "5px";
+    mainContainer.appendChild(newDiv);
 }
 
 /**
@@ -62,9 +34,22 @@ const clearDiv = () => {
 };
 
 const callbackFunction = () => {
-    animationFunction().then(result => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("test");
+            counter++;
+            counter < 5 && animationFunction();
+        }, 1000);
+
+        mainContainer.onclick = () => {
+            counter = 0;
+            clearDiv();
+            callbackFunction();
+        };
+        counter = 0;
+        clearDiv();
         callbackFunction();
-    });
+    })
 }
 
 // First initialization

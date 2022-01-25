c6130a1eab49096c6631bd142ce3982b9437129b
host4ideas <felixmb99@hotmail.es>
2022-01-25 17:14:00 +0100

    started home page

--- Unidad_04/Proyecto Unidad 4/survival_game_phaser/src/game.js
+++ Unidad_04/Proyecto Unidad 4/survival_game_phaser/src/game.js
@@ -1,20 +1,28 @@
 import MainScene from './scenes/MainScene.js'
 
-const config = {
-	type: Phaser.AUTO,
-	parent: 'survival-game',
-	width: 800,
-	height: 600,
-	pixelArt: true,
-	scene: [MainScene],
-	physics: {
-		default: "arcade",
-		arcade: {
-			// debug: true,
-			// debugShowBody: true,
-			gravity: { y: 0 }
-		}
-	},
-};
+const main = () => {
+	console.log("window loaded");
+	const config = {
+		type: Phaser.AUTO,
+		parent: 'survival-game',
+		width: 800,
+		height: 600,
+		pixelArt: true,
+		scene: [MainScene],
+		physics: {
+			default: "arcade",
+			arcade: {
+				// debug: true,
+				// debugShowBody: true,
+				gravity: { y: 0 }
+			}
+		},
+	};
 
-const game = new Phaser.Game(config);
+	$("#startGame").click(() => {
+		console.log("click")
+		const game = new Phaser.Game(config);
+	});
+}
+
+window.onload = main;

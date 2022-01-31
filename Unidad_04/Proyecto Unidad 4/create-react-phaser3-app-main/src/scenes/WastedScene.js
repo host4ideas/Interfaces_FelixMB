344b2f2ecb13d7710a6a29d2f3eaa884424b0ed2
host4ideas <felixmb99@hotmail.es>
2022-01-31 17:33:28 +0100

    Created WastedScene

---
+++ Unidad_04/Proyecto Unidad 4/create-react-phaser3-app-main/src/scenes/WastedScene.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+import wastedVideo from '../assets/video/gtav_wasted.mp4';
+
+export default class WastedScene extends Phaser.Scene {
+	constructor() {
+		super('wastedscene');
+	}
+
+	preload() {
+		this.load.video('wasted', wastedVideo, 'loadeddata', false, true);
+	}
+
+	create() {
+		const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+		const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+		var vid = this.add.video(screenCenterX, screenCenterY, 'wasted');
+
+		vid.play(true);
+
+		// Prevents video freeze when game is out of focus (i.e. user changes tab on the browser)
+		vid.setPaused(false);
+	}
+}
